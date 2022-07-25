f3435ab5f4ff7da6181c95dfce1137f899de667e
ksuryawa <kapil.r.suryawanshi@gmail.com>
2022-07-25 18:17:09 +0100

    Add step definitions for login and home page

---
+++ src/test/java/com/ksuryawa/stepdefinitions/ActiTimeHomePageStepDefinitions.java
@@ -0,0 +1,27 @@
+package com.ksuryawa.stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+/**
+ * @author Kapil Suryawanshi
+ * 25/07/2022
+ */
+public class ActiTimeHomePageStepDefinitions {
+
+	@Then("I should see {string}")
+	public void i_should_see(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@When("I click on the logout button")
+	public void i_click_on_the_logout_button() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Then("I should be logged out of ActiTime application")
+	public void i_should_be_logged_out_of_acti_time_application() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+}
---
+++ src/test/java/com/ksuryawa/stepdefinitions/ActiTimeLoginStepDefinitions.java
@@ -0,0 +1,44 @@
+package com.ksuryawa.stepdefinitions;
+
+import com.ksuryawa.pages.ActiTimeHomePage;
+import com.ksuryawa.pages.ActiTimeLoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+
+/**
+ * @author Kapil Suryawanshi
+ * 25/07/2022
+ */
+public class ActiTimeLoginStepDefinitions {
+
+	@Steps
+	ActiTimeLoginPage loginPage;
+
+	@Steps
+	ActiTimeHomePage homePage;
+
+	@Given("I am on the ActiTime login page")
+	public void i_am_on_the_acti_time_login_page() {
+		loginPage.open();
+	}
+	@When("I enter {string} into the username field")
+	public void i_enter_into_the_username_field(String userName) {
+		loginPage.enterUserName(userName);
+	}
+	@When("I enter {string} into the password field")
+	public void i_enter_into_the_password_field(String password) {
+		loginPage.enterPassword(password);
+	}
+	@When("I click on the login button")
+	public void i_click_on_the_login_button() {
+		loginPage.clickOnLoginButton();
+	}
+	@Then("I should be logged in to ActiTime application")
+	public void i_should_be_logged_in_to_acti_time_application() {
+		homePage.verifyLogo();
+	}
+
+
+}
